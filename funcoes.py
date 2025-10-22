88e6e99b9bff7a7c7dab2eb6e831e5932a6d267e
Joaquim Zanette <quimzanette@gmail.com>
2025-10-22 10:07:04 -0300

    add posiciona_frota

--- funcoes.py
+++ funcoes.py
@@ -20,3 +20,20 @@ def faz_jogada(tabuleiro, linha, coluna):
     else:
         tabuleiro[linha][coluna] = '-'
     return tabuleiro
+
+def posiciona_frota (frota):
+    tabuleiro = []
+    for x in range(10):
+        linha = []
+        for y in range(10):
+            linha.append(0)
+        tabuleiro.append(linha)
+
+    for nome_navio in frota:
+        for navio in frota[nome_navio]: 
+            for posicao in navio:        
+                linha = posicao[0]
+                coluna = posicao[1]
+                tabuleiro[linha][coluna] = 1  
+    
+    return tabuleiro
